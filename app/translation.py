7ac568a9d825d1260d9dfc00a4ac5319697cd867
npsytoh <npsytoh@gmail.com>
2024-10-22 12:12:00 +0900

    feat: Add dotenv

--- app/translation.py
+++ app/translation.py
@@ -1,9 +1,28 @@
+import config
 import deepl
 
 
-def request_translation(text: str, lang: str):
-    auth_key = ""
+def get_connection():
+    auth_key = config.DEEPL_API_KEY
     translator = deepl.Translator(auth_key)
+    return translator
+
+
+def request_translation(text: str, lang: str):
+    try:
+        translator = get_connection()
+    except Exception as e:
+        return f"Error: {e}"
+
+    usage = translator.get_usage()
+    if usage.any_limit_reached:
+        return "Error: request limit reached"
 
     result = translator.translate_text(text, target_lang=lang)
     return result.text
+
+
+def get_usage_count():
+    translator = get_connection()
+    usage = translator.get_usage()
+    return {"count": usage.character.count, "limit": usage.character.limit}
