d79d7ca47c89115d3317271a4fea168294534796
npsytoh <npsytoh@gmail.com>
2024-10-27 00:40:32 +0900

    feat: Add middleware

--- app/main.py
+++ app/main.py
@@ -2,11 +2,24 @@ from typing import Annotated
 
 import uvicorn
 from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.schemas import TranslateParams, TranslateResponse
 from app.translation import request_translation
 
 app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/translate", response_model=TranslateResponse)
